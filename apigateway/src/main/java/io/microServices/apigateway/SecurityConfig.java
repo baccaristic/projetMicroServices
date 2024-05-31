25737ff2d74ea48a3f6f645fa8116052cdb3acd7
tbaccar.ext <tbaccar.ext@diamos.com>
2024-06-01 00:59:19 +0100

    Set up eureka project

---
+++ apigateway/src/main/java/io/microServices/apigateway/SecurityConfig.java
@@ -0,0 +1,47 @@
+package io.microServices.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${keycloak.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer
+                                .jwt(jwt ->
+                                        jwt.jwkSetUri(jwkSetUri)
+                                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                                )
+                );
+        return http.build();
+    }
+
+    @Bean
+    public Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    }
+
+}
